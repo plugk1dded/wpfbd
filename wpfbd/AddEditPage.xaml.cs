1187ae5553d54e466b2de4ec9c51ed593712197a
plugk1dded <plugk1dded@gitbebra.ru>
2025-05-05 15:44:06 +0500

    Add project files.

---
+++ wpfbd/AddEditPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpfbd
+{
+    /// <summary>
+    /// Interaction logic for AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+    {
+        private Hotel _currentHotel = new Hotel();
+        public AddEditPage(Hotel selectedHotel)
+        {
+            InitializeComponent();
+            if (selectedHotel != null)
+                _currentHotel = selectedHotel;
+
+            DataContext = _currentHotel;
+            ComboCountries.ItemsSource = ToursEntities.GetContext().Country.ToList();
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+            var context = ToursEntities.GetContext();
+            var entry = context.Entry<Hotel>(_currentHotel);
+
+            if (string.IsNullOrWhiteSpace(_currentHotel.Name))
+                errors.AppendLine("Укажите название отеля");
+            if (_currentHotel.CountOfStars < 1 || _currentHotel.CountOfStars > 5)
+                errors.AppendLine("Количество звёзд - число от 1 до 5");
+            if (_currentHotel.Country == null)
+                errors.AppendLine("Выберите страну");
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+
+            var countHotel = context.Hotel.ToList().Count();
+
+            if (_currentHotel.Id == 0)
+                _currentHotel.Id = countHotel + 1;
+                context.Hotel.Add(_currentHotel);
+                context.SaveChanges();
+
+            try
+            {
+                context.SaveChanges();
+                MessageBox.Show("Информация сохранена!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+    }
+}
