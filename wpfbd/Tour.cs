9749248034edb99d2a600de79194de3bedd65a79
plugk1dded <plugk1dded@gitbebra.ru>
2025-05-20 17:00:50 +0500

    fr

--- wpfbd/Tour.cs
+++ wpfbd/Tour.cs
@@ -27,7 +27,12 @@ namespace wpfbd
         public byte[] ImagePreview { get; set; }
         public decimal Price { get; set; }
         public bool isActual { get; set; }
-    
+
+        public string ActualText
+        {
+            get { return (isActual ? "Актуален" : "Завершен"); }
+        }
+
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Type> Type { get; set; }
     }
